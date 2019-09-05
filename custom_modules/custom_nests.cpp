944d3ba84a0e76ba1ef7ca5c7eb50470242a6dee
Randy Heiland <heiland@indiana.edu>
2019-09-05 09:45:15 -0400

    track_intern...

--- custom_modules/custom_nests.cpp
+++ custom_modules/custom_nests.cpp
@@ -67,125 +67,193 @@
 
 #include "./custom_nests.h"
 
-// declare cell definitions here 
-Cell_Definition cell0;   // dividing
-Cell_Definition cell1;   // differentiated
+// declare cell definitions here
+Cell_Definition dividing_cell;   // dividing
+Cell_Definition differentiated_cell;   // differentiated
 
 
 void create_cell_types( void )
 {
-	// use the same random seed so that future experiments have the 
-	// same initial histogram of oncoprotein, even if threading means 
-	// that future division and other events are still not identical 
-	// for all runs 
-	
-	SeedRandom( parameters.ints("random_seed") ); // or specify a seed here 
-	
-	
-	// Force 2D sim BEFORE using cell_defaults to define our custom cell types
-	cell_defaults.functions.set_orientation = up_orientation; 
-	cell_defaults.phenotype.geometry.polarity = 1.0;
-	cell_defaults.phenotype.motility.restrict_to_2D = true; 
+	// use the same random seed so that future experiments have the
+	// same initial histogram of oncoprotein, even if threading means
+	// that future division and other events are still not identical
+	// for all runs
 
-	// housekeeping 
+	//SeedRandom( parameters.ints("random_seed") ); // or specify a seed here
+
+
+	// Force 2D sim BEFORE using cell_defaults to define our custom cell types
+	cell_defaults.functions.set_orientation = up_orientation;
+	cell_defaults.phenotype.geometry.polarity = 1.0;
+	cell_defaults.phenotype.motility.restrict_to_2D = true;
+
+	// housekeeping
 	initialize_default_cell_definition();
-	cell_defaults.phenotype.secretion.sync_to_microenvironment( &microenvironment ); 
+	cell_defaults.phenotype.secretion.sync_to_microenvironment( &microenvironment );
+	cell_defaults.phenotype.molecular.sync_to_microenvironment( &microenvironment );
 	cell_defaults.phenotype.sync_to_functions( cell_defaults.functions );
-	
+
+	// no death
+	int apoptosis_model_index = cell_defaults.phenotype.death.find_death_model_index( "Apoptosis" );
+	int necrosis_model_index = cell_defaults.phenotype.death.find_death_model_index( "Necrosis" );
+
+	cell_defaults.phenotype.death.rates[apoptosis_model_index] = 0.0;
+	cell_defaults.phenotype.death.rates[necrosis_model_index] = 0.0;
+
+	// Turn of Cell Cycling
+	int cycle_start_index = live.find_phase_index( PhysiCell_constants::live );
+	int cycle_end_index = live.find_phase_index( PhysiCell_constants::live );
+	cell_defaults.phenotype.cycle.data.transition_rate(cycle_start_index,cycle_end_index) = 0.0;
+
+	int oxygen_ID = microenvironment.find_density_index( "oxygen" ); // 0
+	int inhibitor_ID = microenvironment.find_density_index( "inhibitor" ); // 1
+	std::cout << "----- create_cell_types: oxygen_ID, inhibitor_ID =" << oxygen_ID << ", "<<inhibitor_ID <<std::endl;
+
+
+	cell_defaults.phenotype.secretion.secretion_rates[oxygen_ID] = 10;
+	cell_defaults.phenotype.secretion.uptake_rates[oxygen_ID] = 0;
+	// cell_defaults.phenotype.secretion.saturation_densities[oxygen_ID] = 38;
+	cell_defaults.phenotype.secretion.saturation_densities[oxygen_ID] = 10;
+
+// Move these down *after* they are created/copied from cell_defaults
+	// dividing_cell.phenotype.secretion.secretion_rates[inhibitor_ID] = 0;
+	// differentiated_cell.phenotype.secretion.secretion_rates[inhibitor_ID] = 100;
+	// dividing_cell.phenotype.secretion.uptake_rates[inhibitor_ID] = 100;
+	// differentiated_cell.phenotype.secretion.uptake_rates[inhibitor_ID] = 0;
+	// cell_defaults.phenotype.secretion.saturation_densities[inhibitor_ID] = 100;
 
 	// Define the 2 cell types:
 
 	// --- dividing cells
-	cell0 = cell_defaults;
-	cell0.type = 0;
-	cell0.phenotype.motility.is_motile = true;
-	cell0.phenotype.differentiation.differentiation_possible = true;
-	
+	dividing_cell = cell_defaults;
+	dividing_cell.type = 0;
+	dividing_cell.phenotype.motility.is_motile = true;
+	dividing_cell.phenotype.differentiation.differentiation_possible = true;
+
 	std::vector<double> probabilities;
-	probabilities.push_back(0.5);
-	probabilities.push_back(0.5);
-	cell0.phenotype.differentiation.probabilities = probabilities;
-	
+	probabilities.push_back(0);
+	probabilities.push_back(1);
+	probabilities.push_back(0);
+	dividing_cell.phenotype.differentiation.probabilities = probabilities;
+	dividing_cell.functions.update_phenotype = custom_probability_update;
+
 	std::vector<Differentiation_Outcome> outcomes;
-	Differentiation_Outcome* symmetric_0 = new Differentiation_Outcome(&cell0, &cell0);
-	Differentiation_Outcome* asymmetric_0 = new Differentiation_Outcome(&cell0, &cell1);
+	Differentiation_Outcome* symmetric_0 = new Differentiation_Outcome(&dividing_cell, &dividing_cell);
+	Differentiation_Outcome* asymmetric_0 = new Differentiation_Outcome(&dividing_cell, &differentiated_cell);
+	Differentiation_Outcome* symmetricD_0 = new Differentiation_Outcome(&differentiated_cell, &differentiated_cell);
 	outcomes.push_back(*symmetric_0);
 	outcomes.push_back(*asymmetric_0);
-	cell0.phenotype.differentiation.outcomes = outcomes;
-	
+	outcomes.push_back(*symmetricD_0);
+	dividing_cell.phenotype.differentiation.outcomes = outcomes;
+
 	// --- differentiated cells
-	cell1 = cell_defaults;
-	cell1.type = 1;
-	cell1.phenotype.motility.is_motile = false;
-	cell1.phenotype.differentiation.differentiation_possible = false;
-	
-	return; 
+	differentiated_cell = cell_defaults;
+	differentiated_cell.type = 1;
+	differentiated_cell.phenotype.motility.is_motile = false;
+	differentiated_cell.phenotype.differentiation.differentiation_possible = false;
+	differentiated_cell.functions.update_phenotype = custom_probability_update2;
+
+
+	// rwh: now set secretion and uptake rates - get them from the config file (e.g., config/PhysiCell_settings.xml)
+	//
+	// "non-dividing cells create some sort of inhibitor molecule and subsequently secrete this molecule. 
+	// Then, the dividing cells would uptake this inhibitor and the probability that these dividing cells differentiate 
+	// into non-dividing cells would vary with the concentration/absolute number of “inhibitor” molecules consumed.
+	dividing_cell.phenotype.secretion.secretion_rates[inhibitor_ID] = parameters.doubles("dividing_cell_inhibitor_secretion");
+	differentiated_cell.phenotype.secretion.secretion_rates[inhibitor_ID] = parameters.doubles("differentiated_cell_inhibitor_secretion");
+
+	dividing_cell.phenotype.secretion.uptake_rates[inhibitor_ID] = parameters.doubles("dividing_cell_inhibitor_uptake");
+	differentiated_cell.phenotype.secretion.uptake_rates[inhibitor_ID] = parameters.doubles("differentiated_cell_inhibitor_uptake");
+	// cell_defaults.phenotype.secretion.saturation_densities[inhibitor_ID] = 100;
+	std::cout << "\n------- sanity check inhibitor_ID rates:" <<std::endl;
+	std::cout << "       dividing secretion=" << dividing_cell.phenotype.secretion.secretion_rates[inhibitor_ID] <<std::endl;
+	std::cout << "       dividing uptake=" << dividing_cell.phenotype.secretion.uptake_rates[inhibitor_ID] <<std::endl;
+	std::cout << "       differentiated secretion=" << differentiated_cell.phenotype.secretion.secretion_rates[inhibitor_ID] <<std::endl;
+	std::cout << "       differentiated uptake=" << differentiated_cell.phenotype.secretion.uptake_rates[inhibitor_ID] <<std::endl;
+	std::cout << "----------------------------\n" <<std::endl;
+
+
+	return;
+
+
 }
 
 void setup_microenvironment( void )
 {
-	// make sure to override and go back to 2D 
+	// make sure to override and go back to 2D
 	if( default_microenvironment_options.simulate_2D == false )
 	{
-		std::cout << "Warning: overriding XML config option and setting to 2D!" << std::endl; 
-		default_microenvironment_options.simulate_2D = true; 
+		std::cout << "Warning: overriding XML config option and setting to 2D!" << std::endl;
+		default_microenvironment_options.simulate_2D = true;
 	}
-	
-/* 
-	// no gradients need for this example 
-	default_microenvironment_options.calculate_gradients = false; 
-	
-	// set Dirichlet conditions 
+
+
+
+/*
+	// no gradients need for this example
+	default_microenvironment_options.calculate_gradients = false;
+
+	// set Dirichlet conditions
 	default_microenvironment_options.outer_Dirichlet_conditions = true;
-	
-	// if there are more substrates, resize accordingly 
+
+	// if there are more substrates, resize accordingly
 	std::vector<double> bc_vector( 1 , 38.0 ); // 5% o2
 	default_microenvironment_options.Dirichlet_condition_vector = bc_vector;
 */
-
-/* 
+/*
 sets the custom rule (custom_cell_rule) to NULL and update phenotype function
 (update_phenotype) to update_cell_and_death_parameters_O2_based, so the cell changes its cycle entry rate and
 necrosis rate according to its local oxygenation conditions. (See Section 17.6.)
 */
 
+	/*
+	cell1.phenotype.secretion.secretion_rates[Dfac_ID] = 0;
+ 	cell0.phenotype.secretion.secretion_rates[Dfac_ID] = 0;
+	cell1.phenotype.secretion.uptake_rates[Dfac_ID] = 0;
+	cell0.phenotype.secretion.uptake_rates[Dfac_ID] = 20;
+	cell_defaults.phenotype.secretion.saturation_densities[Dfac_ID] = 100;
 
-	// cell_defaults.phenotype.secretion.secretion_rates[0] = 0; 
-	cell_defaults.phenotype.secretion.secretion_rates[0] = 10; 
-	// cell_defaults.phenotype.secretion.uptake_rates[0] = 10; 
-	cell_defaults.phenotype.secretion.uptake_rates[0] = 0; 
-	cell_defaults.phenotype.secretion.saturation_densities[0] = 38; 
-	cell_defaults.phenotype.secretion.saturation_densities[0] = 10; 
-
-	default_microenvironment_options.calculate_gradients = false;
+	default_microenvironment_options.calculate_gradients = true;
 	default_microenvironment_options.track_internalized_substrates_in_each_agent = true;
 	default_microenvironment_options.outer_Dirichlet_conditions = false;
+	*/
 	// default_microenvironment_options.outer_Dirichlet_conditions = true;
 
 	// default_microenvironment_options.use_oxygen_as_first_field = true;
+	/*
 	default_microenvironment_options.use_oxygen_as_first_field = false;
 	microenvironment.set_density(0, "signal", "dimensionless" );
 	microenvironment.decay_rates[0] = 0; 	 // no decay
-	microenvironment.diffusion_coefficients[0] = 0.5; 	
+	microenvironment.diffusion_coefficients[0] = 0.5;
 
-	std::vector<double> bc_vector( 1 , 38  ); 
-	
-	// default_microenvironment_options.Dirichlet_condition_vector[0] = 38; // physioxic conditions 
-	// default_microenvironment_options.Dirichlet_activation_vector[0] = false;  
-	
-	// initialize BioFVM 
-	initialize_microenvironment(); 	
+	default_microenvironment_options.initial_condition_vector = { 100.0, 0.0, 0.0 };
+	*/
+	//std::vector<double> bc_vector( 1 , 38  );
 
+	// default_microenvironment_options.Dirichlet_condition_vector[0] = 38; // physioxic conditions
+	// default_microenvironment_options.Dirichlet_activation_vector[0] = false;
+
+
+	// parameters.doubles("dividing_cell_inhibitor_secretion");
+	// parameters.bools("dividing_cell_inhibitor_secretion");
+	// default_microenvironment_options.track_internalized_substrates_in_each_agent = parameters.bools("track_internalized_substrates_in_each_agent");
+	// std::cout << "default_microenvironment_options.track_internalized_substrates_in_each_agent " << default_microenvironment_options.track_internalized_substrates_in_each_agent << std::endl;
+	default_microenvironment_options.track_internalized_substrates_in_each_agent = true;
+
+	// initialize BioFVM
+	initialize_microenvironment();
+
+	/*
 	double factor = 1.0;
 	for( unsigned int n=0; n < microenvironment.number_of_voxels() ; n++ )
 	{
 		// factor = 1.0 - ((n%20)/20.);
 		// bc_vector[0] = factor*38.;
 		bc_vector[0] = 0.0;
-		microenvironment(n) = bc_vector; 
-	}	
-	
-	return; 
+		microenvironment(n) = bc_vector;
+	}
+	*/
+	return;
 }
 
 void setup_tissue( void )
@@ -193,15 +261,15 @@ void setup_tissue( void )
 	Cell* pC;
 
 	// create a single cell, at the origin
-	pC = create_cell( cell0 );
+	pC = create_cell( dividing_cell );
 	pC->assign_position( 0.0, 0.0, 0.0 );
 
 	// create some cells near the origin
-/* 	
-	double cell_radius = cell_defaults.phenotype.geometry.radius; 
-	double cell_spacing = 0.95 * 2.0 * cell_radius; 
-	
-	double nest_radius = 60; 
+/*
+	double cell_radius = cell_defaults.phenotype.geometry.radius;
+	double cell_spacing = 0.95 * 2.0 * cell_radius;
+
+	double nest_radius = 60;
 	for(int i = -nest_radius; i < nest_radius; i+=cell_spacing)
 	{
 		for(int j = -nest_radius; j < nest_radius; j+=15){
@@ -213,25 +281,53 @@ void setup_tissue( void )
 			}
 		}
 	}
-*/	
-	return; 
+*/
+	return;
 }
 
 std::vector<std::string> my_coloring_function( Cell* pCell )
 {
-	std::vector<std::string> output = false_cell_coloring_cytometry(pCell); 
-		
+	std::vector<std::string> output = false_cell_coloring_cytometry(pCell);
+
 	if(pCell->type == 0 )
 	{
-		 output[0] = "green"; 
-		 output[2] = "green"; 
+		 output[0] = "green";
+		 output[2] = "green";
 	} else if(pCell->type == 1 ){
-		 output[0] = "red"; 
-		 output[2] = "red"; 
+		 output[0] = "red";
+		 output[2] = "red";
 	} else if(pCell->type == 2 ){
-		 output[0] = "blue"; 
-		 output[2] = "blue"; 
+		 output[0] = "blue";
+		 output[2] = "blue";
 	}
-	
-	return output; 
+
+	return output;
+}
+
+
+void custom_probability_update(Cell* pCell, Phenotype& phenotype, double dt)
+{
+	static int inhibitor_i = microenvironment.find_density_index("inhibitor");
+	double inhibitor_Con = phenotype.molecular.internalized_total_substrates[inhibitor_i];
+	//pCell -> advance_bundled_phenotype_functions( dt);
+	//phenotype.secretion.advance(pCell, phenotype, dt);
+	double SymR = (8)/(10 + 10*inhibitor_Con);
+	double Asym = 1 - SymR;
+
+	std::vector<double> probabilities{ SymR, 0 , Asym };
+	phenotype.differentiation.probabilities = probabilities;
+	return;
+
+}
+
+void custom_probability_update2(Cell* pCell, Phenotype& phenotype, double dt)
+{
+	static int inhibitor_i = microenvironment.find_density_index("inhibitor");
+	phenotype.molecular.internalized_total_substrates[inhibitor_i] += 1.0;
+  double inhibitor_Con2 = phenotype.molecular.internalized_total_substrates[inhibitor_i];
+	//phenotype.secretion.advance(pCell, phenotype, dt);
+	//pCell -> advance_bundled_phenotype_functions( dt);
+	std::cout << inhibitor_Con2 << " ";
+  return;
+
 }
--- main_nests.cpp
+++ main_nests.cpp
@@ -74,126 +74,128 @@
 #include <fstream>
 
 #include "./core/PhysiCell.h"
-#include "./modules/PhysiCell_standard_modules.h" 
+#include "./modules/PhysiCell_standard_modules.h"
 
-// put custom code modules here! 
+// put custom code modules here!
 
-#include "./custom_modules/custom_nests.h" 
-	
+#include "./custom_modules/custom_nests.h"
+
 using namespace BioFVM;
 using namespace PhysiCell;
 
 int main( int argc, char* argv[] )
 {
 	// load and parse settings file(s)
-	
-	bool XML_status = false; 
+
+	bool XML_status = false;
 	if( argc > 1 )
 	{ XML_status = load_PhysiCell_config_file( argv[1] ); }
 	else
 	{ XML_status = load_PhysiCell_config_file( "./config/PhysiCell_settings.xml" ); }
 	if( !XML_status )
 	{ exit(-1); }
-	
+
 	// OpenMP setup
 	omp_set_num_threads(PhysiCell_settings.omp_num_threads);
-	
-	// time setup 
-	std::string time_units = "min"; 
 
-	/* Microenvironment setup */ 
-	
-	setup_microenvironment(); // modify this in the custom code 
-	
-	/* PhysiCell setup */ 
- 	
+	SeedRandom( parameters.ints("random_seed") ); 
+
+	// time setup
+	std::string time_units = "min";
+
+	/* Microenvironment setup */
+
+	setup_microenvironment(); // modify this in the custom code
+
+	/* PhysiCell setup */
+
 	// set mechanics voxel size, and match the data structure to BioFVM
-	double mechanics_voxel_size = 30; 
+	double mechanics_voxel_size = 30;
 	Cell_Container* cell_container = create_cell_container_for_microenvironment( microenvironment, mechanics_voxel_size );
-	
-	/* Users typically start modifying here. START USERMODS */ 
-	
+
+	/* Users typically start modifying here. START USERMODS */
+
 	create_cell_types();
 	setup_tissue();
 
-	/* Users typically stop modifying here. END USERMODS */ 
-	
-	// set MultiCellDS save options 
+	/* Users typically stop modifying here. END USERMODS */
 
-	set_save_biofvm_mesh_as_matlab( true ); 
-	set_save_biofvm_data_as_matlab( true ); 
-	set_save_biofvm_cell_data( true ); 
+	// set MultiCellDS save options
+
+	set_save_biofvm_mesh_as_matlab( true );
+	set_save_biofvm_data_as_matlab( true );
+	set_save_biofvm_cell_data( true );
 	set_save_biofvm_cell_data_as_custom_matlab( true );
-	
-	// save a simulation snapshot 
-	
+
+	// save a simulation snapshot
+
 	char filename[1024];
-	sprintf( filename , "%s/initial" , PhysiCell_settings.folder.c_str() ); 
-	save_PhysiCell_to_MultiCellDS_xml_pugi( filename , microenvironment , PhysiCell_globals.current_time ); 
-	
-	// save a quick SVG cross section through z = 0, after setting its 
-	// length bar to 200 microns 
+	sprintf( filename , "%s/initial" , PhysiCell_settings.folder.c_str() );
+	save_PhysiCell_to_MultiCellDS_xml_pugi( filename , microenvironment , PhysiCell_globals.current_time );
 
-	PhysiCell_SVG_options.length_bar = 200; 
+	// save a quick SVG cross section through z = 0, after setting its
+	// length bar to 200 microns
 
-	// for simplicity, set a pathology coloring function 
-	
-	std::vector<std::string> (*cell_coloring_function)(Cell*) = my_coloring_function; 
-	
-	sprintf( filename , "%s/initial.svg" , PhysiCell_settings.folder.c_str() ); 
+	PhysiCell_SVG_options.length_bar = 200;
+
+	// for simplicity, set a pathology coloring function
+
+	std::vector<std::string> (*cell_coloring_function)(Cell*) = my_coloring_function;
+
+	sprintf( filename , "%s/initial.svg" , PhysiCell_settings.folder.c_str() );
 	SVG_plot( filename , microenvironment, 0.0 , PhysiCell_globals.current_time, cell_coloring_function );
-	
-	display_citations(); 
-	
-	// set the performance timers 
+
+	display_citations();
+
+	// set the performance timers
 
 	BioFVM::RUNTIME_TIC();
 	BioFVM::TIC();
-	
+
 	std::ofstream report_file;
 	if( PhysiCell_settings.enable_legacy_saves == true )
-	{	
-		sprintf( filename , "%s/simulation_report.txt" , PhysiCell_settings.folder.c_str() ); 
-		
-		report_file.open(filename); 	// create the data log file 
+	{
+		sprintf( filename , "%s/simulation_report.txt" , PhysiCell_settings.folder.c_str() );
+
+		report_file.open(filename); 	// create the data log file
 		report_file<<"simulated time\tnum cells\tnum division\tnum death\twall time"<<std::endl;
 	}
-	
-	// main loop 
-	try 
-	{		
+
+	// main loop
+	try
+	{
 		while( PhysiCell_globals.current_time < PhysiCell_settings.max_time + 0.1*diffusion_dt )
 		{
-			// save data if it's time. 
+			// save data if it's time.
 			if( fabs( PhysiCell_globals.current_time - PhysiCell_globals.next_full_save_time ) < 0.01 * diffusion_dt )
 			{
-				display_simulation_status( std::cout ); 
+				display_simulation_status( std::cout );
 				if( PhysiCell_settings.enable_legacy_saves == true )
-				{	
+				{
 					log_output( PhysiCell_globals.current_time , PhysiCell_globals.full_output_index, microenvironment, report_file);
 				}
-				
+
 				if( PhysiCell_settings.enable_full_saves == true )
-				{	
-					sprintf( filename , "%s/output%08u" , PhysiCell_settings.folder.c_str(),  PhysiCell_globals.full_output_index ); 
-					
-					save_PhysiCell_to_MultiCellDS_xml_pugi( filename , microenvironment , PhysiCell_globals.current_time ); 
+				{
+					sprintf( filename , "%s/output%08u" , PhysiCell_settings.folder.c_str(),  PhysiCell_globals.full_output_index );
+
+					save_PhysiCell_to_MultiCellDS_xml_pugi( filename , microenvironment , PhysiCell_globals.current_time );
 				}
-				
-				PhysiCell_globals.full_output_index++; 
+
+				PhysiCell_globals.full_output_index++;
 				PhysiCell_globals.next_full_save_time += PhysiCell_settings.full_save_interval;
 			}
-			
+
 			// save SVG plot if it's time
 			if( fabs( PhysiCell_globals.current_time - PhysiCell_globals.next_SVG_save_time  ) < 0.01 * diffusion_dt )
 			{
-				// std::cout << PhysiCell_globals.current_time << ", " << PhysiCell_globals.next_SVG_save_time << std::endl; 
+				// std::cout << PhysiCell_globals.current_time << ", " << PhysiCell_globals.next_SVG_save_time << std::endl;
 				if (int(PhysiCell_globals.current_time) % (24*60) == 0)
 				{
-					std::cout << "------> dividing " << std::endl; 
+					std::cout << "------> dividing " << std::endl;
 					// if( PhysiCell_globals.current_time % (24*60)  ) < 0.01 * diffusion_dt )
 					Cell* pC = NULL;
-					// for (auto & pC : all_cells) 
+					// for (auto & pC : all_cells)
 					for( int i=0; i < (*all_cells).size(); i++ )
 					{
 						pC = (*all_cells)[i];
@@ -206,11 +208,11 @@ int main( int argc, char* argv[] )
 				}
 
 				if( PhysiCell_settings.enable_SVG_saves == true )
-				{	
-					sprintf( filename , "%s/snapshot%08u.svg" , PhysiCell_settings.folder.c_str() , PhysiCell_globals.SVG_output_index ); 
+				{
+					sprintf( filename , "%s/snapshot%08u.svg" , PhysiCell_settings.folder.c_str() , PhysiCell_globals.SVG_output_index );
 					SVG_plot( filename , microenvironment, 0.0 , PhysiCell_globals.current_time, cell_coloring_function );
-					
-					PhysiCell_globals.SVG_output_index++; 
+
+					PhysiCell_globals.SVG_output_index++;
 					PhysiCell_globals.next_SVG_save_time  += PhysiCell_settings.SVG_save_interval;
 
 				}
@@ -218,19 +220,19 @@ int main( int argc, char* argv[] )
 
 			// update the microenvironment
 			microenvironment.simulate_diffusion_decay( diffusion_dt );
-			
-			// run PhysiCell 
+
+			// run PhysiCell
 			((Cell_Container *)microenvironment.agent_container)->update_all_cells( PhysiCell_globals.current_time );
-			
+
 			/*
-			  Custom add-ons could potentially go here. 
+			  Custom add-ons could potentially go here.
 			*/
-			
+
 			PhysiCell_globals.current_time += diffusion_dt;
 		}
-		
+
 		if( PhysiCell_settings.enable_legacy_saves == true )
-		{			
+		{
 			log_output(PhysiCell_globals.current_time, PhysiCell_globals.full_output_index, microenvironment, report_file);
 			report_file.close();
 		}
@@ -239,20 +241,19 @@ int main( int argc, char* argv[] )
 	{ // reference to the base of a polymorphic object
 		std::cout << e.what(); // information from length_error printed
 	}
-	
-	// save a final simulation snapshot 
-	
-	sprintf( filename , "%s/final" , PhysiCell_settings.folder.c_str() ); 
-	save_PhysiCell_to_MultiCellDS_xml_pugi( filename , microenvironment , PhysiCell_globals.current_time ); 
-	
-	sprintf( filename , "%s/final.svg" , PhysiCell_settings.folder.c_str() ); 
+
+	// save a final simulation snapshot
+
+	sprintf( filename , "%s/final" , PhysiCell_settings.folder.c_str() );
+	save_PhysiCell_to_MultiCellDS_xml_pugi( filename , microenvironment , PhysiCell_globals.current_time );
+
+	sprintf( filename , "%s/final.svg" , PhysiCell_settings.folder.c_str() );
 	SVG_plot( filename , microenvironment, 0.0 , PhysiCell_globals.current_time, cell_coloring_function );
-	
-	// timer 
-	
-	std::cout << std::endl << "Total simulation runtime: " << std::endl; 
-	BioFVM::display_stopwatch_value( std::cout , BioFVM::runtime_stopwatch_value() ); 
 
-	return 0; 
+	// timer
+
+	std::cout << std::endl << "Total simulation runtime: " << std::endl;
+	BioFVM::display_stopwatch_value( std::cout , BioFVM::runtime_stopwatch_value() );
+
+	return 0;
 }
-
